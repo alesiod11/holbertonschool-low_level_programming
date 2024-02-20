59af4c2dcfc4c0007fbb854d1273b0726ea453e8
alesiod11 <8970@holbertonstudents.com>
2024-02-20 11:13:09 +0000

    exercise

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -20,10 +20,10 @@ printf("%d is positive\n", n);
 if (n == 0)
 {
 printf("%d is zero\n", n);
-    }
-    if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
-    return (0);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
