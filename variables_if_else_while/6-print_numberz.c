2ce5bca4cafe9498cb279005adda9b9959a22420
alesiod11 <8970@holbertonstudents.com>
2024-02-20 12:58:18 +0000

    exercise

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include<stdlib.h>
+
+/**
+ * main - main function
+ * Return: 0 value
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
