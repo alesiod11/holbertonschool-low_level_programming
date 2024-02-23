40ddbe9328321e94a03940a28eae41bfb2a2f659
alesiod11 <8970@holbertonstudents.com>
2024-02-23 14:32:34 +0000

    Alphabet x10

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ * Return: void
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	} else
+	{
+		return 0;
+	}
+}
