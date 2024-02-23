9f6bbad51b39c6159c24f86c65f18c66820ff823
alesiod11 <8970@holbertonstudents.com>
2024-02-23 10:26:51 +0000

    exercise

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
+#include"main.h"
 
-void print_alphabet_x10(void) {
-    char letter = 'a';
-    int i, j;
-    
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 26; j++) {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-        letter = 'a'; // Reset letter to 'a' for next iteration
-    }
-}
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
+ */
 
-int main() {
-    print_alphabet_x10();
-    return 0;
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
 }
