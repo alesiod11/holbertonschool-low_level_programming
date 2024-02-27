e5b103a3560f8f2a8eb462d3bbe53f58cf1c4304
alesiod11 <8970@holbertonstudents.com>
2024-02-27 13:02:11 +0000

    exercise

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - finds length of string
+ * @s: string to find length
+ * Return: returns length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
