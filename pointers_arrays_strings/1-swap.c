33b04da611f78408d4019f371a8516d1237f1ec2
alesiod11 <8970@holbertonstudents.com>
2024-02-27 12:50:16 +0000

    exercise

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * 1-swap - swaps the value of two integers
+ * swap_int - swaps the value of two integers
  * @a: value of first number
  * @b: value of second number
  */
-
 void swap_int(int *a, int *b)
 {
 	int tmp;
