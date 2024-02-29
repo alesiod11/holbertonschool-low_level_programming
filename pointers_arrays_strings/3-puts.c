7179bb51da6608e3ac4d0955013253290d9d8036
alesiod11 <8970@holbertonstudents.com>
2024-02-29 20:15:32 +0000

    exercise

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,19 +1,19 @@
-#include "main.h"
-#include "stdio.h"
+#include "main.h" // Assuming _putchar is declared in main.h
+#include <stdio.h>
+
 /**
  * _puts - prints a string
  * @str: string
  */
 void _puts(char *str)
 {
-	int l;
+    int l;
 
-	l = 0;
-	while (str[l] != '\0')
-	{
-		_putchar(str[l]);
-		l++;
-	}
-	_putchar('\n');
+    l = 0;
+    while (str[l] != '\0')
+    {
+        _putchar(str[l]); // Call _putchar from main.h
+        l++;
+    }
+    _putchar('\n'); // Call _putchar from main.h
 }
-/* _putchar(str[l])  _putchar(*(str + l)) */
