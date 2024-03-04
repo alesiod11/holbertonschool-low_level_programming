20b9c0345ea3843898747b48e081b60576120ab7
alesiod11 <8970@holbertonstudents.com>
2024-03-04 09:27:04 +0000

    exercise

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half -  print the second half of the string
+ * @str: input string
+ */
+ void puts_half(char *str)
+ {
+    int len, idx;
+
+    len = 0;
+
+    while (str[len]!= '\0')
+    len ++;
+    
+    if (len % 2 == 0)
+        idx = len / 2;
+    else 
+        idx = (len + 1) / 2;
+
+    while(str[idx] != '\0')
+    {
+        _putchar(str[idx]);
+    }
+ }
