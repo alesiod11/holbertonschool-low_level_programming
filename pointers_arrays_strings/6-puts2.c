1993da7ccaf737b4020b68439500bfcbe07021bb
alesiod11 <8970@holbertonstudents.com>
2024-03-05 10:45:57 +0000

    exercise

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
- * puts2 -  prints every other character of a string
+ * puts2 - prints every other character of a string
  * @str: input string
  */
 void puts2(char *str)
- {
-    int i;
+{
+    int i = 0;
 
-    i = 0;
     while (str[i] != '\0')
     {
         if (i % 2 == 0)
             _putchar(str[i]);
+        i++;
     }
 }
