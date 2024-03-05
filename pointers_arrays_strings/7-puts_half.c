a290e2230385c47d2d21d19df592198063c0c90c
alesiod11 <8970@holbertonstudents.com>
2024-03-05 10:56:36 +0000

    exercise

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,13 +13,13 @@ while (str[len] != '\0')
 len++;
 if (len % 2 == 0)
 		idx = len / 2;
-    else
-        idx = (len + 1) / 2;
+else
+		idx = (len + 1) / 2;
 
-    while(str[idx] != '\0')
-    {
-        _putchar(str[idx]);
-        idx++;
-    }
-        _putchar('\n');
- }
+while (str[idx] != '\0')
+{
+		_putchar(str[idx]);
+		idx++;
+}
+		_putchar('\n');
+}
