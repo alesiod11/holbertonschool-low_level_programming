c61f8fa16eec2d8613e732be5ebbb3053cb7e3fd
alesiod11 <8970@holbertonstudents.com>
2024-03-05 10:39:37 +0000

    exercise

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,13 +7,13 @@
  */
 void _puts(char *str)
 {
-    int l;
+int l;
 
-    l = 0;
-    while (str[l] != '\0')
-    {
-        _putchar(str[l]);
-        l++;
-    }
-    _putchar('\n');
+l = 0;
+while (str[l] != '\0')
+{
+_putchar(str[l]);
+l++;
+}
+_putchar('\n');
 }
