2a30d8f28d27569489dec2c83e9ee4318a65c458
alesiod11 <8970@holbertonstudents.com>
2024-03-06 13:05:59 +0000

    exercise

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,1 +1,14 @@
 #include "main.h"
+/**
+*_strlen_recursion - prints a string
+*@s: string
+*Return : NULL
+*/
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        return 0;
+    }
+    return 1 + _strlen_recursion(s + 1);
+}
